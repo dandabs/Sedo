274431fcc78c6516e614924142a05ca9962543f3
Daniel Adams <danstanswinxp@gmail.com>
2020-04-19 13:08:04 +0100

    Removed verified members counter

--- events/ready.js
+++ events/ready.js
@@ -28,7 +28,7 @@ module.exports = (client) => {
     setInterval(() => {
 
       client.guilds.get('529631776625131520').channels.get('701387399523074109').setName("Discord Members: " + client.guilds.get('529631776625131520').members.size);
-      client.guilds.get('529631776625131520').channels.get('701386093475397724').setName("Verified Members: " + client.guilds.get('529631776625131520').roles.get('535134591087018001').members.size);
+      //client.guilds.get('529631776625131520').channels.get('701386093475397724').setName("Verified Members: " + client.guilds.get('529631776625131520').roles.get('535134591087018001').members.size);
 
       axios.get("https://groups.roblox.com/v1/groups/4430859")
       .then(response => {
