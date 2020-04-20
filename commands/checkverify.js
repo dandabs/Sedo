51394225bd79c000c0924186187b04bf6297d369
Daniel Adams <danstanswinxp@gmail.com>
2020-04-20 17:14:46 +0100

    i keep making mistakes

--- commands/checkverify.js
+++ commands/checkverify.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 var axios = require('axios');
 
 var admin = require('firebase-admin');
-var serviceAccount = require("./helsinkicruises-firebase-adminsdk-35i3k-deb8a8b7ab.json");
+var serviceAccount = require("../helsinkicruises-firebase-adminsdk-35i3k-deb8a8b7ab.json");
 var db = admin.firestore();
 
 exports.run = (client, message, args) => {
